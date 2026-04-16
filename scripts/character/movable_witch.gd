97287620b2c74ee23cdb64956031194b480f577a
lucasjanta <lucastyle2014@gmail.com>
2026-04-15 22:27:14 -0300

    new assets and character movement

---
+++ scripts/character/movable_witch.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+@export var speed := 150.0
+const JUMP_VELOCITY = -400.0
+var dir := Vector2(0.0, 0.0) 
+
+func _physics_process(delta: float) -> void:
+
+	dir.x = Input.get_axis("left", "right")
+	dir.y = Input.get_axis("up", "down")
+	
+	if dir != Vector2.ZERO:
+		animation_player.play("walk")
+	else:
+		animation_player.stop()
+	
+	if dir.y < 0:
+		animated_sprite_2d.animation = "back"
+	elif dir.y > 0:
+		animated_sprite_2d.animation = "front"
+	
+	velocity = dir.normalized() * speed
+	
+	move_and_slide()
